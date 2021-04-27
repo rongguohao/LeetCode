4cf6039ffcfe7af416e86e98c23b0c21a6938004
rongguohao <rongguohao@vip56.cn>
2021-04-27 15:05:45 +0800

    1

--- 两数相加/两数相加/Program.cs
+++ 两数相加/两数相加/Program.cs
@@ -69,6 +69,12 @@ namespace 两数相加
 
     public class Solution
     {
+        /// <summary>
+        /// 迭代法
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
             int val = 0;
@@ -85,5 +91,33 @@ namespace 两数相加
             }
             return prenode.next;
         }
+
+        ///// <summary>
+        ///// 计算两个链表相加结果：递归法
+        ///// </summary>
+        ///// <param name="l1">链表1</param>
+        ///// <param name="l2">链表2</param>
+        ///// <param name="num">进位<param>
+        ///// <returns>相加结果逆序链表</returns>
+        //public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int num = 0)
+        //{
+        //    /*递归边界：两个链表均为空*/
+        //    if (l1 == null && l2 == null)
+        //    {
+        //        if (num != 0) return new ListNode(num);
+        //        else return default;
+        //    }
+
+        //    /*计算两链表当前节点值相加之和：节点1值+节点2值+进位值*/
+        //    int midValue = (l1?.val ?? 0) + (l2?.val ?? 0) + num;
+
+        //    /*创建节点储存相加之和非进位部分*/
+        //    ListNode listNode = new ListNode(midValue % 10);
+
+        //    /*两链表指针右移，继续计算。*/
+        //    listNode.next = AddTwoNumbers(l1?.next, l2?.next, midValue / 10);
+
+        //    return listNode;
+        //}
     }
 }
