9bebe69b0169627c0492dc8f49a9e40b0be118d4
rongguohao <rongguohao@vip56.cn>
2021-05-06 10:17:32 +0800

    1

--- 寻找两个正序数组的中位数/Program.cs
+++ 寻找两个正序数组的中位数/Program.cs
@@ -26,7 +26,7 @@ namespace 寻找两个正序数组的中位数
 
     //输入：nums1 = [2], nums2 = []
     //    输出：2.00000
- 
+
     //提示：
 
     //nums1.length == m
@@ -37,6 +37,7 @@ namespace 寻找两个正序数组的中位数
     //-106 <= nums1[i], nums2[i] <= 106
 
     //进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+    //https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/leetcode-4-median-of-two-sorted-arrays-xun-zhao-li/
     class Program
     {
         static void Main(string[] args)
@@ -44,6 +45,36 @@ namespace 寻找两个正序数组的中位数
             Console.WriteLine("Hello World!");
 
             Console.ReadKey();
+        }
+
+
+        static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int n = nums1.Length;
+            int m = nums2.Length;
+            int len = n + m;
+            int kPre = (len + 1) / 2;
+            int k = (len + 2) / 2;
+            if (len % 2 == 0)
+                return (GetKth(nums1, 0, n - 1, nums2, 0, m - 1, kPre) + GetKth(nums1, 0, n - 1, nums2, 0, m - 1, k)) * 0.5;
+            else
+                return GetKth(nums1, 0, n - 1, nums2, 0, m - 1, k);
+        }
+
+        static int GetKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k)
+        {
+            int len1 = end1 - start1 + 1;
+            int len2 = end2 - start2 + 1;
+            //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1 
+            if (len1 > len2) return GetKth(nums2, start2, end2, nums1, start1, end1, k);
+            if (len1 == 0) return nums2[start2 + k - 1];
+            if (k == 1) return Math.Min(nums1[start1], nums2[start2]);
+            int i = start1 + Math.Min(len1, k / 2) - 1;
+            int j = start2 + Math.Min(len2, k / 2) - 1;
+            if (nums1[i] > nums2[j])
+                return GetKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+            else
+                return GetKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
         }
     }
 }
---
+++ 最长回文子串/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace 最长回文子串
+{
+    //给你一个字符串 s，找到 s 中最长的回文子串。
+
+
+
+    //示例 1：
+
+    //输入：s = "babad"    
+    //输出："bab"
+    //解释："aba" 同样是符合题意的答案。
+    //示例 2：
+
+    //输入：s = "cbbd"
+    //输出："bb"
+    //示例 3：
+
+    //输入：s = "a"
+    //输出："a"
+    //示例 4：
+
+    //输入：s = "ac"
+    //输出："a"
+    //https://leetcode-cn.com/problems/longest-palindromic-substring/solution/leetcode-5-longest-palindromic-substring-zui-chang/
+
+    //回文串（palindromic string）是指这个字符串无论从左读还是从右读，所读的顺序是一样的；简而言之，回文串是左右对称的。所谓最长回文子串问题，是指对于一个给定的母串
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LongestPalindrome("bacabad");
+            Console.ReadKey();
+        }
+
+        //穷举法
+        //时间复杂度: 判断是否为回文串的复杂度为O(n)，longestPalindrome有双层for循环，因此总的复杂度为为O(n^3)
+        //空间复杂度：O(1)
+        static string LongestPalindrome(string s)
+        {
+            string result = "";
+            int n = s.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    // 检查 s[i]到s[j]是否是回文串，如果是，且长度大于result长度，就更新它
+                    int p = i, q = j;
+                    bool isPalindromic = true;
+                    while (p < q)
+                    {
+                        var a = s[p++];
+                        var b = s[q--];
+                        if (a != b)
+                        {
+                            isPalindromic = false;
+                            break;
+                        }
+                    }
+                    if (isPalindromic)
+                    {
+                        int len = j - i + 1;
+                        if (len > result.Length)
+                        {
+                            result = s.Substring(i, len);
+                        }
+                    }
+
+                }
+            }
+            return result;
+        }
+    }
+}
